711930dde0da701965a55bbb8a7d097e54ed92cf
HajatAli777 <hajatbagoro@gmail.com>
2025-09-25 04:54:50 -0700

    create login screen

---
+++ lib/components/Splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:app/components/Login_form.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    navigationToLoginScreen();
+    super.initState();
+  }
+
+  void navigationToLoginScreen() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => LoginForm()),
+        (route) => false,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Image.asset()));
+  }
+}
---
+++ lib/utills/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class Appcolors{
+  static const primaryColar = Color(0xff347433);
+  static const secondaryColor = Color(0xffFFC107);
+  static const boldText = Color(0xff1D1D1D);
+  static const textColor = Color(0xff999999);
+
+}
