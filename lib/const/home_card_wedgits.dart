66db0c44eaabee56a60039c89b0aae8a23408829
HajatAli777 <hajatbagoro@gmail.com>
2025-10-09 02:04:28 -0700

    Refactor HomeCardWidget and Homepage to improve code readability and maintainability; update cardList structure and utilize dynamic properties for rendering.

--- lib/const/home_card_wedgits.dart
+++ lib/const/home_card_wedgits.dart
@@ -1,11 +1,7 @@
-
 import 'package:app/const/toldText.dart';
 import 'package:app/utills/colors.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-
 import 'package:flutter/material.dart';
-
-
 
 class HomeCardWidget extends StatelessWidget {
   final String imageUrl;
@@ -14,258 +10,136 @@ class HomeCardWidget extends StatelessWidget {
   final int reviews;
   final int price;
 
-  const HomeCardWidget({super.key, required this.imageUrl, required this.title, required this.rating, required this.reviews, required this.price});
+  const HomeCardWidget({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.rating,
+    required this.reviews,
+    required this.price,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadiusGeometry.circular(12),
+      borderRadius: BorderRadius.circular(12),
       child: Container(
-        height: 700,
-        width: double.infinity,
-        
-        decoration: BoxDecoration(color: AppColors.secondaryColor, 
+        margin: const EdgeInsets.only(bottom: 16), 
+        decoration: BoxDecoration(
+          color: AppColors.secondaryColor,
+          borderRadius: BorderRadius.circular(12),
         ),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXBhcnRtZW50JTIwaW50ZXJpb3J8ZW58MHx8MHx8fDA%3D",
-                      ),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 30,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            color: AppColors.subtitleTextColor.withValues(
-                              alpha: 0.2,
-                            ),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Center(
-                            child: Text('Most Recommended'),
-                          ),
-                        ),
-                      ],
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                const SizedBox(height: 12),
-                Row(
-                  children: [
-                    Text('Hamilton Studio Appartment', style: TextStyle(color: AppColors.toldTextColor, fontWeight: FontWeight.bold),),
-                    RatingBar.builder(
-                      initialRating: 5,
-                      minRating: 1,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      tapOnlyMode: true,
-                      updateOnDrag: false,
-                      ignoreGestures: true,
-                      itemCount: 5,
-                      itemSize: 24,
-                      itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-                      itemBuilder: (context, _) =>
-                          Icon(Icons.star, color: AppColors.primaryColor),
-                      onRatingUpdate: (rating) {
-                        print(rating);
-                      },
-                    ),
-                    Text("(20)", style: TextStyle(color: AppColors.subtitleTextColor)),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "10 km away from your location",
-                    style: TextStyle(color: AppColors.subtitleTextColor),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: AppColors.primaryColor,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "\$10 a night",
-                          style: TextStyle(color: AppColors.secondaryColor),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppColors.subtitleTextColor.withValues(alpha: 0.2),
-                      ),
-                      child: Center(child: Icon(Icons.favorite_border)),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
-                  height: 400,
-                  // color: AppColors.C_color,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: double.infinity,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                                  image: DecorationImage(
-                      image: NetworkImage(
-                        "https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww",
-                      ),
-                      fit: BoxFit.fill,
-                         
-                        )
-                           
-                            
-                          ),
-
-                      
-                        ),
-                        const SizedBox(height: 10,),
-
-                        Row(
-                          children: [
-                            Text('Befar Apartment', style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w300,
-                            ),),
-                            const SizedBox(width:120),
-                            RatingBar.builder(
-                      initialRating: 5,
-                      minRating: 1,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      tapOnlyMode: true,
-                      updateOnDrag: false,
-                      ignoreGestures: true,
-                      itemCount: 5,
-                      itemSize: 24,
-                      itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-                      itemBuilder: (context, _) =>
-                          Icon(Icons.star, color: AppColors.primaryColor),
-                      onRatingUpdate: (rating) {
-                        print(rating);
-                      },
-                    ),
-                    Text('(20)', style: TextStyle(
-                      color: AppColors.subtitleTextColor.withValues(alpha: 0.8),
-                    ),),
-
-
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                         Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "10 km away from your location",
-                    style: TextStyle(color: AppColors.subtitleTextColor,
-                    fontSize: 20,
-                    
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColor, 
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(
-                        child: Text('\$12 a night', style: TextStyle(
-                          color: AppColors.secondaryColor,
-                        
-                        ),),
-                      ),
-                    ),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: AppColors.subtitleTextColor.withValues(alpha: 0.2)
-                      ),
-                      child: Icon(Icons.favorite_border_rounded),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  width: 100,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: AppColors.primaryColor,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child:Row(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox(width: 14,),
-                      Icon(Icons.map_outlined, color: AppColors.secondaryColor),
-                      Center(
-                        child: Text('map', style: TextStyle(
-                          color: AppColors.secondaryColor,
-                          fontSize: 20,
-                        ),),
+                      Container(
+                        height: 30,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: AppColors.subtitleTextColor.withOpacity(0.2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Center(
+                          child: Text('Most Recommended'),
+                        ),
                       ),
                     ],
                   ),
-                  
-                  
-                )
-
-
-
-
-                        
-                        
-                        
-                        
-                      ],
+                ),
+              ),
+              const SizedBox(height: 12),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        color: AppColors.toldTextColor,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-            
-                )
-            
-              ],
-            ),
+                  RatingBar.builder(
+                    initialRating: rating,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    tapOnlyMode: true,
+                    updateOnDrag: false,
+                    ignoreGestures: true,
+                    itemCount: 5,
+                    itemSize: 24,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: AppColors.primaryColor,
+                    ),
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    },
+                  ),
+                  Text(
+                    "($reviews)",
+                    style: TextStyle(color: AppColors.subtitleTextColor),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              // Distance
+              Text(
+                "10 km away from your location",
+                style: TextStyle(color: AppColors.subtitleTextColor),
+              ),
+              const SizedBox(height: 16),
+              // Price and Favorite Icon
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: AppColors.primaryColor,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "\$$price a night",
+                        style: TextStyle(color: AppColors.secondaryColor),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: AppColors.subtitleTextColor.withOpacity(0.2),
+                    ),
+                    child: const Center(child: Icon(Icons.favorite_border)),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -12,54 +12,55 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   List cardList = [
     {
-      'imageUrl':'https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXBhcnRtZW50JTIwaW50ZXJpb3J8ZW58MHx8MHx8fDA%3D',
+      'imageUrl':
+          'https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXBhcnRtZW50JTIwaW50ZXJpb3J8ZW58MHx8MHx8fDA%3D',
       'title': 'Luxury Apartment',
       'rating': 4.9,
       'reviews': 400,
-      'price': 11000,
-
+      'price': 100,
     },
     {
-      'imageUrl':'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww',
+      'imageUrl':
+          'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWwlMjByb29tfGVufDB8fDB8fHww',
       'title': 'The Grand Hotel',
       'rating': 3.6,
       'reviews': 370,
-      'price': 5499,
-
+      'price': 76,
     },
     {
-      'imageUrl':'https://holopakistan.com/wp-content/uploads/2022/10/1407953244000-177513283.jpg',
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo73dlbBQ1J4gZJuH2fwwWr4mWl0uIenxC8Q&s',
       'title': 'Family Apartment',
-      'rating': 4,
+      'rating': 4.0,
       'reviews': 330,
-      'price': 7000,
-
+      'price': 70,
     },
     {
-      'imageUrl':'https://www.millenniumhotels.com/mhb-media/asia/13534-m-hotel-singapore/rooms/new-images/m-hotel.jpg?rev=9e50aad8af644f5387339ec40d324fb4',
+      'imageUrl':
+          'https://www.millenniumhotels.com/mhb-media/asia/13534-m-hotel-singapore/rooms/new-images/m-hotel.jpg?rev=9e50aad8af644f5387339ec40d324fb4',
       'title': 'Room with a view',
       'rating': 3.4,
       'reviews': 300,
-      'price': 4300,
-
+      'price': 43,
     },
     {
-      'imageUrl':'https://www.travelandleisure.com/thmb/fXDn5hZ59eUXAMNtiyRQBe2Oh2g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-mandarin-oreintal-new-york-NYCHOTELVIEWS0924-830f2725a84e4bc1a5203acb12023018.jpg',
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9P6cL784vcHpdMvYPXkdkFYdTvoo5puSfg&s',
       'title': 'Royal Hotel',
       'rating': 4.6,
       'reviews': 450,
-      'price': 5000,
-
+      'price': 50,
     },
     {
-      'imageUrl':'https://media.cnn.com/api/v1/images/stellar/prod/4b-mandarin-oriental-jumeira-dubai-the-royal-penthouse-bedroom.jpg?q=w_1110,c_fill',
+      'imageUrl':
+          'https://media.cnn.com/api/v1/images/stellar/prod/4b-mandarin-oriental-jumeira-dubai-the-royal-penthouse-bedroom.jpg?q=w_1110,c_fill',
       'title': 'The Ramada Hotel',
-      'rating': 5,
+      'rating': 5.0,
       'reviews': 1200,
-      'price': 12000,
-
+      'price': 120,
     },
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +70,7 @@ class _HomepageState extends State<Homepage> {
           child: SingleChildScrollView(
             child: Column(
               children: [
+                // Header
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -81,15 +83,13 @@ class _HomepageState extends State<Homepage> {
                       height: 40,
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
-                        color:
-                            AppColors.subtitleTextColor.withOpacity(0.2), // Fixed alpha
+                        color: AppColors.subtitleTextColor.withOpacity(0.2),
                       ),
                       child: const Icon(Icons.notification_important_outlined),
-                    )
+                    ),
                   ],
                 ),
                 const SizedBox(height: 20),
-            
                 // Search Box
                 Container(
                   height: 56,
@@ -97,7 +97,7 @@ class _HomepageState extends State<Homepage> {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(24),
                     border: Border.all(
-                      color: AppColors.subtitleTextColor.withOpacity(0.4), // Fixed alpha
+                      color: AppColors.subtitleTextColor.withOpacity(0.4),
                     ),
                   ),
                   child: Padding(
@@ -118,7 +118,6 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ),
                 const SizedBox(height: 15),
-            
                 // Property Type Icons Row
                 SizedBox(
                   height: 80,
@@ -134,29 +133,24 @@ class _HomepageState extends State<Homepage> {
                       Image.asset('assets/bungalows.png'),
                     ],
                   ),
-                  
                 ),
                 const SizedBox(height: 20),
-                // HomeCardWidget(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: ListView.builder(itemCount: cardList.length, 
-                  itemBuilder: (context, index){
+                // List of Cards
+                ListView.builder(
+                  shrinkWrap: true, // Allows ListView to take only needed space
+                  physics: const NeverScrollableScrollPhysics(), // Disable ListView scrolling
+                  itemCount: cardList.length,
+                  itemBuilder: (context, index) {
                     var cardListData = cardList[index];
                     return HomeCardWidget(
                       imageUrl: cardListData['imageUrl'],
                       title: cardListData['title'],
-                      rating: cardListData['rating'],
+                      rating: cardListData['rating'].toDouble(),
                       reviews: cardListData['reviews'],
                       price: cardListData['price'],
-
                     );
-                    
-
-                  }
-                )
-            
-                )
+                  },
+                ),
               ],
             ),
           ),
