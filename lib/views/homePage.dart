951830999711b347c7bb7323357cb3a6035a1e65
HajatAli777 <hajatbagoro@gmail.com>
2025-10-22 10:34:32 -0700

    class and object

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:app/const/home_card_wedgits.dart';
+import 'package:app/controllers/home_controller.dart';
 import 'package:app/utills/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:app/views/apartment_booking_screen.dart';
@@ -12,136 +13,14 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  List cardList = [
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000',
-      'title': 'Luxury Apartment',
-      'rating': 4.9,
-      'reviews': 400,
-      'price': 100,
-      'distance': 12,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '3 Guest',
-      'beds': '3 Beds',
-      'bathroom': '1 Bathroom',
-      'bookingDate': "1 - 16 Dec",
-      "stayDays": "16 Days"
-
-    },
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000',
-      'title': 'The Grand Hotel',
-      'rating': 3.6,
-      'reviews': 370,
-      'price': 76,
-      'distance': 34,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '4 Guest',
-      'beds': '4 Beds',
-      'bathroom': '1 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo73dlbBQ1J4gZJuH2fwwWr4mWl0uIenxC8Q&s',
-      'title': 'Family Apartment',
-      'rating': 4.0,
-      'reviews': 330,
-      'price': 70,
-      'distance': 20,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '2 Guest',
-      'beds': '2 Beds',
-      'bathroom': '1 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000',
-      'title': 'MIdway ',
-      'rating': 4.8,
-      'reviews': 40,
-      'price': 108,
-      'distance': 42,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '6 Guest',
-      'beds': '6 Beds',
-      'bathroom': '3 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000',
-      'title': 'KFC',
-      'rating': 3.6,
-      'reviews': 90,
-      'price': 5,
-      'distance': 4,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '5 Guest',
-      'beds': '6 Beds',
-      'bathroom': '1 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo73dlbBQ1J4gZJuH2fwwWr4mWl0uIenxC8Q&s',
-      'title': 'Park Hotel',
-      'rating': 4.0,
-      'reviews': 630,
-      'price': 90,
-      'distance': 23,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '2 Guest',
-      'beds': '2 Beds',
-      'bathroom': '1 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?fm=jpg&q=60&w=3000',
-      'title': 'Rupal in',
-      'rating': 4.9,
-      'reviews': 400,
-      'price': 100,
-      'distance': 12,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '2 Guest',
-      'beds': '2 Beds',
-      'bathroom': '1 Bathroom'
-    },
-    {
-      'imageUrl':
-          'https://plus.unsplash.com/premium_photo-1661964402307-02267d1423f5?fm=jpg&q=60&w=3000',
-      'title': 'Sareena Hotel',
-      'rating': 3.6,
-      'reviews': 30,
-      'price': 56,
-      'distance':43,
-      'places': 'Pakistan,  india,  America,  Japan  ',
-      'guest': '4 Guest',
-      'beds': '4 Beds',
-      'bathroom': '1 Bathroom'
-      
-    },
-    {
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo73dlbBQ1J4gZJuH2fwwWr4mWl0uIenxC8Q&s',
-      'title': 'Ramada Hotel',
-      'rating': 4.0,
-      'reviews': 330,
-      'price': 73,
-      'distance': 21,
-      'places': 'Turkey,  india,  America,  Japan  ',
-      'guest': '2 Guest',
-      'beds': '2 Beds',
-      'bathroom': '1 Bathroom'
-    },
-  ];
-
+  HomeController homeController = HomeController();
   List cardListData = [];
-
+  List cardList = [];
   @override
   void initState() {
     super.initState();
-    cardListData = List.from(cardList);
+    cardListData = List.from(homeController.cardList);
+    cardList = List.from(homeController.cardList);
   }
 
   void search(String query) {
@@ -229,14 +108,13 @@ class _HomepageState extends State<Homepage> {
                 ),
 
                 const SizedBox(height: 20),
-
-                // List of Cards
                 ListView.builder(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   itemCount: cardList.length,
                   itemBuilder: (context, index) {
                     var cardItem = cardList[index];
+                    
                     return HomeCardWidget(
                       imageUrl: cardItem['imageUrl'],
                       title: cardItem['title'],
