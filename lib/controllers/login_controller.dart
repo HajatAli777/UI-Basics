c5a02128c4f880cebd4282f35e42ca26a2e3c37f
HajatAli777 <hajatbagoro@gmail.com>
2025-10-24 07:52:52 -0700

    sp

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,26 +1,49 @@
 import 'package:app/views/profileSetup_screen.dart';
 import 'package:flutter/material.dart';
 
-class LoginController {
-  void loginUser(String password,String email, BuildContext context){
-    try{
-      if (email.isEmpty || password.isEmpty){
-        throw Exception("Email or password cannot empty");
+// class LoginController {
+//   void loginUser(String password,String email, BuildContext context){
+//     try{
+//       if (email.isEmpty || password.isEmpty){
+//         throw Exception("Email or password cannot empty");
 
-      }
-      else{
-        debugPrint("login sucefully");
-        Navigator.push(
-                                context, MaterialPageRoute(
-                                  builder: (context) => ProfilesetupScreen()
-                                  )
-                                  );
-      }
-    }catch(e){
-      debugPrint("This is Error$e");
-    }
+//       }
+//       else{
+//         debugPrint("login sucefully");
+//         Navigator.push(
+//                                 context, MaterialPageRoute(
+//                                   builder: (context) => ProfilesetupScreen()
+//                                   )
+//                                   );
+//       }
+//     }catch(e){
+//       debugPrint("This is Error$e");
+//     }
     
-  }
+//   }
   
 
+// }
+class LoginController {
+  void loginUser(String password, String email, BuildContext context){
+    password= '122323';
+    email= 'ali324';
+    try{
+      if(password.isEmpty || email.isEmpty){
+        debugPrint("Passward and Email cannot be empty!");
+        
+      }
+      else {
+        password=password;
+        email=email;
+        debugPrint("Login sucessfully ");
+      }
+
+    }catch(e){
+      debugPrint("This is Error: $e");
+
+
+    }
+
+  }
 }
