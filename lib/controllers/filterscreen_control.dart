d4be6588646d07755fb45cbad440a1ca87b6443c
HajatAli777 <hajatbagoro@gmail.com>
2025-10-29 10:02:15 -0700

    dd

---
+++ lib/controllers/filterscreen_control.dart
@@ -0,0 +1,9 @@
+import 'package:get/state_manager.dart';
+
+class filterScreenControll extends GetxController{
+  
+
+  
+
+
+}
--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 class SplashController extends GetxController{
   @override
   void onInit() {
-    // TODO: implement onInit
+   
     super.onInit();
     navigationToLoginScreen();
   }
