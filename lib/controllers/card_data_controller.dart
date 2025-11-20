66ac97067aaa16c5151ff7fb0feed4b0743cc1c9
HajatAli777 <hajatbagoro@gmail.com>
2025-11-20 04:55:05 -0800

    Enhance CardDataController and CardData view: implement image upload functionality, improve image selection, and update UI to display selected images.

--- lib/controllers/card_data_controller.dart
+++ lib/controllers/card_data_controller.dart
@@ -1,12 +1,19 @@
 import 'dart:io';
+import 'package:app/const/loading_dialog.dart';
+import 'package:app/models/card_model.dart';
+import 'package:app/views/bottom_navbar_screens.dart/card_Data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CardDataController extends GetxController {
+  var firestore = FirebaseFirestore.instance;
+  var storage = FirebaseFirestore.instance;
   var selectedImage = Rxn<File>();
   var _imagePicker = ImagePicker();
+  var uuid = Uuid().v1();
   Future<void> pickImage() async {
     try {
       final pickedImage = await _imagePicker.pickImage(
@@ -17,6 +24,36 @@ class CardDataController extends GetxController {
       }
     } catch (e) {
       debugPrint("this is the error:$e");
+    }
+  }
+  Future<String?> uploadImage() async {
+    try {
+      final fileName = DateTime.now().microsecondsSinceEpoch.toString();
+      final storageRef = storage.child('cardImages/$fileName');
+      final uploadTask = storageRef.putFile(selectedImage.value!);
+      final snapShot = await uploadTask;
+      final downLoadUrl = await snapShot.ref.getDownloadURL();
+      return downLoadUrl;
+    } catch (e) {
+      debugPrint("Error uploading image: $e");
+      return null;
+    }
+  }
+  void uploadToFirestore() async{
+    try{
+      Get.dialog(LoadingDialog(), barrierDismissible: false);
+      final imageUrl = await uploadImage();
+      if(imageUrl != null){
+        final cardData = CardModel(
+          imageUrl: imageUrl, title: 'Ramada', places: 'the place', rating: 4.5, reviews: 100, guest: 'guest', beds: '4', bathroom: '1', bookingDate: '25/2/2004', stayDays: '3', price: 20,
+        );
+        await firestore.collection("cardData").doc()
+        Get.back(); 
+        Get.snackbar("Success", "Card data uploaded successfully");
+
+      }
+    }catch(e){
+      debugPrint("Error uploading data to Firestore: $e");
     }
   }
 }
--- lib/views/bottom_navbar_screens.dart/card_Data.dart
+++ lib/views/bottom_navbar_screens.dart/card_Data.dart
@@ -1,5 +1,7 @@
+import 'package:app/const/primarybutton.dart';
 import 'package:app/controllers/card_data_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/instance_manager.dart';
 
 class CardData extends StatelessWidget {
@@ -11,26 +13,31 @@ class CardData extends StatelessWidget {
     return Scaffold(
       body: SafeArea(child: Column(
         children: [
-          Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50,),
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
+          Obx(
+            ()=> Container(
+              width: double.infinity,
+              height: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50,),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                image: DecorationImage(image: cardDataController.selectedImage.value !=null ? FileImage(cardDataController.selectedImage.value!):NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/330px-Placeholder_view_vector.svg.png"),fit: BoxFit.cover)
+                
               ),
-              image: DecorationImage(image: cardDataController.selectedImage.value !=null ? FileImage(cardDataController.selectedImage.value!):NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/330px-Placeholder_view_vector.svg.png"),fit: BoxFit.cover)
+              child: Center(
+                child: IconButton(onPressed: (){
+                  cardDataController.pickImage();
+                }, icon: Icon(Icons.camera_alt,size: 30,color: Colors.grey,)),
+              ),
+            
               
             ),
-            child: Center(
-              child: IconButton(onPressed: (){
-                cardDataController.pickImage();
-              }, icon: Icon(Icons.camera_alt,size: 30,color: Colors.grey,)),
-            ),
-          
+          ),
+          PrimaryButton(buttontext: "Upload Image", onTap: (){
             
-          )
+          })
         ],
       )),
     );
